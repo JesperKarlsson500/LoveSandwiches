940863dc685c10ecd2bb754795953ddca9ecc8b7
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-07-08 21:32:38 +0000

    add sales data to worksheet

--- run.py
+++ run.py
@@ -20,19 +20,34 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    get sales imput figures from the user
+    get sales imput figures from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
-    print("please enter sales data from the last market.")
-    print("data should be six numbers, seperated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        # runs a while loop, that asks the user for data
+        print("please enter sales data from the last market.")
+        print("data should be six numbers, seperated by commas")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("input you data here:")
-    #print(f"the data provided is {data_str}")
+        data_str = input("input you data here:")
+        # print(f"the data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        # It converts the string of data from the user into a list of values
+        # ("1", "2", "3", "4", "5", "6")
+
+        if validate_data(sales_data):
+            # And then we use a single if statement to call our validate data function.
+            # Passing it our sales data list.
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 def validate_data(values):
+    # This function checks for errors. If there are no errors, it will return True
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
@@ -48,6 +63,20 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-    
+        return False
 
-get_sales_data()
+    return True
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
